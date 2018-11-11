c48f3980b5b61eae8a8cef31b7109d42e7f77d1d
Luis Cordero <info@luiscordero29.com>
2018-11-11 16:37:08 -0300

    Validar formulario de login en Ionic 4

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
+import validator from 'validator';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  loginForm = new FormGroup({
+    userEmail: new FormControl(''),
+    userPassword: new FormControl(''),
+  });
+
+  loginFormValidator = {
+    userEmail: {
+      empty: '',
+      email: '',
+    },
+    userPassword: {
+      empty: '',
+    }
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  formValidator(): boolean {
+    if (validator.isEmpty(this.loginForm.value.userEmail)) {
+      this.loginFormValidator.userEmail.empty = 'La dirección de correo es requerido';
+      return false;
+    } else {
+      this.loginFormValidator.userEmail.empty = '';
+    }
+    if (!validator.isEmail(this.loginForm.value.userEmail)) {
+      this.loginFormValidator.userEmail.email = 'Ingrese una dirección de correo válida';
+      return false;
+    } else {
+      this.loginFormValidator.userEmail.email = '';
+    }
+    if (validator.isEmpty(this.loginForm.value.userPassword)) {
+      this.loginFormValidator.userPassword.empty = 'La contraseña es requerido';
+      return false;
+    } else {
+      this.loginFormValidator.userPassword.empty = '';
+    }
+    return true;
+  }
+
+  onSubmit() {
+    if (this.formValidator()) {
+      console.log('Formulario Validado');
+    }
+  }
 }
